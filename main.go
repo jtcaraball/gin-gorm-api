9d005e9e732406c6638b5b81d192386d76d4b6a5
jt caraball <jtcaraballm@gmail.com>
2025-01-18 20:47:52 -0300

    refact: update main.go to match auth/handler changes.

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"gin-gorm-api/handler"
+	"gin-gorm-api/middleware"
+	"gin-gorm-api/model"
+	"gin-gorm-api/provider"
 	"gin-gorm-api/server"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
+
+func startServer(r *gin.Engine) {
+	// Create server with timeout
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+		// set timeout due CWE-400 - Potential Slowloris Attack
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	config, err := server.LoadConfig()
@@ -11,12 +32,29 @@ func main() {
 		panic(err)
 	}
 
-	r, err := server.NewEngine(config, handler.Greeter{})
+	db, err := server.ConnectDB(config)
+	if err != nil {
+		panic(err)
+	}
+	if err = model.RunMigration(db); err != nil {
+		panic(err)
+	}
+
+	mailer := server.NewMailer(config)
+	auth, err := provider.NewUserAuthManager(db, config.Secret, "user", mailer)
+	if err != nil {
+		panic(err)
+	}
+	sm := middleware.NewSessionMiddleware(auth)
+
+	r, err := server.NewEngine(
+		config,
+		handler.NewAuthHandler(auth, sm),
+		handler.NewUserHandler(db, sm),
+	)
 	if err != nil {
 		panic(err)
 	}
 
-	if err = r.Run(); err != nil {
-		panic(err)
-	}
+	startServer(r)
 }
